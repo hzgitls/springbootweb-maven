2c5393855d50cfbed2e554456d48c66ae9d86996
hzgitls <13735560295@163.com>
2020-06-23 07:55:03 +0800

    初始化项目

---
+++ src/main/java/com/lisong/springbootwebmaven/SpringbootwebMavenApplication.java
@@ -0,0 +1,13 @@
+package com.lisong.springbootwebmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootwebMavenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootwebMavenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lisong/springbootwebmaven/SpringbootwebMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lisong.springbootwebmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootwebMavenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
